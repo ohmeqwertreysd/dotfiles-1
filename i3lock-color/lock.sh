96bfc723b22c505554576727377c6ed38d6dbe9e
bob8677 <68208828+bob8677@users.noreply.github.com>
2020-07-20 10:25:04 -0500

    Add files via upload

---
+++ i3lock-color/lock.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+B='#00000000'  # blank
+C='#88c0d033'  # clear ish
+D='#5e81acff'  # default
+T='#5e81acff'  # text
+W='#bf616aff'  # wrong
+V='#a3be8cff'  # verifying
+
+/home/kyle/i3lock-color/x86_64-pc-linux-gnu/i3lock \
+--insidevercolor=$C   \
+--ringvercolor=$V     \
+\
+--insidewrongcolor=$C \
+--ringwrongcolor=$W   \
+\
+--insidecolor=$B      \
+--ringcolor=$D        \
+--linecolor=$B        \
+--separatorcolor=$D   \
+\
+--verifcolor=$T        \
+--wrongcolor=$T        \
+--timecolor=$T        \
+--datecolor=$T        \
+--layoutcolor=$T      \
+--keyhlcolor=$W       \
+--bshlcolor=$W        \
+\
+--screen 1            \
+--blur 5              \
+--indicator           \
+--timestr="%H:%M:%S"  \
+--datestr="%A, %m %Y" \
+--keylayout 2         \
+
+# --veriftext="Drinking verification can..."
+# --wrongtext="Nope!"
+# --textsize=20
+# --modsize=10
+# --timefont=comic-sans
+# --datefont=monofur
+# etc
